c6a4a7bbbdbaa22f6aa63029d8bebb73eb78ea36
clair <clair@159.31.66.156>
2023-10-13 15:55:45 +0200

    implémentation des méthodes de l'interface dans le model

--- Calculatrice/src/genielogiciel/CalculatorModel.java
+++ Calculatrice/src/genielogiciel/CalculatorModel.java
@@ -1,7 +1,66 @@
 package genielogiciel;
 import java.util.Stack;
 
-public class CalculatorModel extends CalculatorModelInterface {
+public class CalculatorModel implements CalculatorModelInterface {
 	public double accu;
 
+	@Override
+	public double add() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double substract() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double multiply() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double divide() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double opposite() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public void push() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void pop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void drop() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void swap() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void clear() {
+		// TODO Auto-generated method stub
+		
+	}
 }
