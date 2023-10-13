333f38ddaf5da7cf8d9259a4eae2a0cb97fd8446
clair <clair@159.31.66.156>
2023-10-13 15:36:12 +0200

    création de la classe CalculatorModel et de l'interface CalculatorModelInterface

---
+++ Calculatrice/src/genielogiciel/CalculatorModel.java
@@ -0,0 +1,7 @@
+package genielogiciel;
+import java.util.Stack;
+
+public class CalculatorModel extends CalculatorModelInterface {
+	public double accu;
+
+}
---
+++ Calculatrice/src/genielogiciel/CalculatorModelInterface.java
@@ -0,0 +1,15 @@
+package genielogiciel;
+
+public interface CalculatorModelInterface {
+	public double add();
+	public double substract();
+	public double multiply();
+	public double divide();
+	public double opposite();
+	public void push();
+	public void pop();
+	public void drop();
+	public void swap();
+	public void clear();
+
+}
