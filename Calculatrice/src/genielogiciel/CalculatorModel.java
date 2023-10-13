f46e720c9a18125eaec0e2114b90d958283b33a7
clair <clair@159.31.66.156>
2023-10-13 16:18:28 +0200

    ajout de commentaires

--- Calculatrice/src/genielogiciel/CalculatorModel.java
+++ Calculatrice/src/genielogiciel/CalculatorModel.java
@@ -5,31 +5,31 @@ public class CalculatorModel implements CalculatorModelInterface {
 	public double accu;
 
 	@Override
-	public double add(double a, double b) {
+	public double add(double a, double b) { // méthode qui additionne deux nombres
 		double somme = a+b;
 		return somme;
 	}
 
 	@Override
-	public double substract(double a, double b) {
+	public double substract(double a, double b) { // méthode qui fait la différence de deux nombres : le deuxième nombre est soustrait au premier
 		double difference = a-b;
 		return difference;
 	}
 
 	@Override
-	public double multiply(double a, double b) {
+	public double multiply(double a, double b) { // méthode qui multiplie deux nombres
 		double produit = a*b;
 		return produit;
 	}
 
 	@Override
-	public double divide(double a, double b) {
+	public double divide(double a, double b) { // méthode qui divise deux nombres : le deuxième nombre divise le premier
 		double quotient = a/b;
 		return quotient;
 	}
 
 	@Override
-	public double opposite(double a) {
+	public double opposite(double a) { // méthode qui oppose un nombre (le multiplie par -1)
 		double oppose = -1*a;
 		return oppose;
 	}
