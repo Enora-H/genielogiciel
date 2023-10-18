30e9408b426c28e47c166bd0b98dc4e82bb0e88e
clair <clair@159.31.66.156>
2023-10-18 14:45:01 +0200

    Ecriture du debut du contenu de CalculatorGUI

--- Blabla/src/genielogiciel/CalculatorGUI.java
+++ Blabla/src/genielogiciel/CalculatorGUI.java
@@ -1,5 +1,32 @@
 package genielogiciel;
 
-public class CalculatorGUI {
+import java.util.List;
+import java.util.Stack;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class CalculatorGUI implements CalculatorGUIInterface {
+
+	@Override
+	public void affiche() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public double change(String accu) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Stack<Double> change(List<Double> stackData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
