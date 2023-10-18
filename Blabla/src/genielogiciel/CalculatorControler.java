eec1affbe603f10cb4652c50f7fb071e9f0d9816
clair <clair@159.31.66.156>
2023-10-18 15:07:59 +0200

    Ajout des méthodes de CalculatorControlerInterface et implémentation de ces méthodes dans CalculatorControler

--- Blabla/src/genielogiciel/CalculatorControler.java
+++ Blabla/src/genielogiciel/CalculatorControler.java
@@ -1,5 +1,20 @@
 package genielogiciel;
 
-public class CalculatorControler {
+import java.util.List;
+import java.util.Stack;
+
+public class CalculatorControler implements CalculatorControlerInterface {
+	
+	@Override
+	public double change(String accu) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public Stack<Double> change(List<Double> stackData) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
--- Blabla/src/genielogiciel/CalculatorControlerInterface.java
+++ Blabla/src/genielogiciel/CalculatorControlerInterface.java
@@ -1,5 +1,7 @@
 package genielogiciel;
 
 public interface CalculatorControlerInterface {
-
+	
+	public double change(String accu);
+	public Stack<Double> change(List<Double> stackData);
 }
