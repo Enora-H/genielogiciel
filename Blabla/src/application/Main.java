3a227b9481a4f185096663f167c16e0d54b78ac7
Enora-H <enora.hllc@free.fr>
2023-10-23 17:04:26 +0200

    Ajouts de boutons sur l'interface

--- Blabla/src/application/Main.java
+++ Blabla/src/application/Main.java
@@ -1,5 +1,8 @@
 package application;
 	
+import genielogiciel.CalculatorControler;
+import genielogiciel.CalculatorGUI;
+import genielogiciel.CalculatorModel;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
@@ -7,19 +10,37 @@ import javafx.scene.layout.BorderPane;
 
 
 public class Main extends Application {
-	@Override
+
+
+	/**
+     * Méthode qui configure et affiche l'interface utilisateur
+     *
+     * @param primaryStage Fenêtre principale qui permet la communication avec l'utilisateur.
+     */
+    @Override
 	public void start(Stage primaryStage) {
 		try {
 			BorderPane root = new BorderPane();
 			Scene scene = new Scene(root,400,400);
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
+				
+	        CalculatorModel model = new CalculatorModel();
+	        CalculatorGUI view = new CalculatorGUI(primaryStage);
+	        //CalculatorControler controller = new CalculatorControler(model, view);
+
+	        primaryStage.setTitle("Calculatricky");
+
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
 	}
 	
+	/**
+     * Méthode main
+     *
+     * @param args Tableau présent par convention pour respecter la signature de main.
+     */
 	public static void main(String[] args) {
 		launch(args);
 	}
--- Blabla/src/genielogiciel/CalculatorGUI.java
+++ Blabla/src/genielogiciel/CalculatorGUI.java
@@ -1,16 +1,36 @@
 package genielogiciel;
 
 import java.util.List;
-import java.util.Stack;
-
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.scene.layout.StackPane;
+import javafx.scene.control.Button;
+import java.util.Stack;
 
 public class CalculatorGUI implements CalculatorGUIInterface {
-
+	public Stage primaryStage;
+	public Scene scene;
+	
+	public CalculatorGUI(Stage primaryStage) {		
+		this.primaryStage = primaryStage;
+		
+		Button button1 = new Button();
+		button1.setText("Test");
+		
+		Button button2 = new Button();
+		button2.setText("2");
+		
+        StackPane layout = new StackPane();           
+        layout.getChildren().addAll(button1, button2);
+        scene = new Scene(layout, 300, 250);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        
+	}
+	
+	
 	@Override
 	public void affiche() {
 		// TODO Auto-generated method stub
