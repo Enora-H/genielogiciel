a6925db3ca25012a844eceb58f4d698af9358c81
clair <clair@10.134.35.246>
2023-10-24 22:42:08 +0200

    Ajout d'une méthode qui fait la conversion List<String> to Stack<Double>

--- Blabla/src/genielogiciel/CalculatorControler.java
+++ Blabla/src/genielogiciel/CalculatorControler.java
@@ -34,24 +34,42 @@ public class CalculatorControler implements CalculatorControlerInterface {
 		pile.addAll(stackData);
 		return pile;
 	}
+	
+	/**
+	 * Méthode qui convertit une liste de chaînes de caractères en une pile de double lisible par le model.
+	 *
+	 * @param inputs Liste de chaînes de caractères à convertir
+	 */
+	public Stack<Double> changeStr(List<String> inputs) {
+	    Stack<Double> pile = new Stack<Double>();
+	    
+	    for (String input : inputs) {
+	        double convertedValue = change(input);
+	        pile.push(convertedValue);
+	    }
+	    
+	    return pile;
+	}
 
 	/**
      *Méthode qui permet d'effectuer une opération selon l'opérateur entré sur un bouton
-     *d'ailleurs faut voir le lien avec ça!!!!!!!
      *
      * @param operation String qui détermine quel type d'opération est à appliquer
      */
-	private double faireOperation(String operation) throws CustomException {// pas encore sûre (on retourne le resultat ou pas?)
-	    List<Double> liste = vue.getStackData(); // implémenter le getter dans la vue!!!!!
-	    model.setPile(change(liste));
+	private List<String> faireOperation(String operation) throws CustomException {
+	    List<String> liste = vue.getStackData(); // implémenter le getter dans la vue!!!!!
+	    model.setPile(changeStr(liste)); // implémenter le setter dans la vue !!!!!
 	    double a = model.pop(); // on prend le dernier élément de la pile
 	    double b = 0.0; // on initialise b à zéro pour ne pas perdre une information de la pile au cas où on ne l'utilise pas
 
+	    liste.remove(liste.size() - 1); // on retire le dernier élément de la liste de String pour plus tard le remplacer par le résultat de l'opération
+	    
 	    if (!"+/-".equals(operation)) { // cas où on n'oppose pas : on vérifie s'il y a assez d'opérandes et on initialise b à la bonne valeur
 	    	if (model.getPile().isEmpty()) {
                 throw new CustomException("Pas assez d'opérandes sur la pile"); // il faut ajouter des "try-catch" dans la vue
             }
 	    	b = model.pop();
+	    	liste.remove(liste.size() - 1); // dans le cas où l'opération n'est pas une opposition, on retire les deux dernières opérandes de la liste de String
 	    }
 
 	    double resultat = 0.0; // on initialise le résultat 
@@ -83,7 +101,9 @@ public class CalculatorControler implements CalculatorControlerInterface {
 	    }
 	    
 	    model.push(resultat); // on pousse l'accumulateur en haut de la pile
-	    return resultat;
+	    String resultatString = Double.toHexString(resultat);
+	    liste.add(resultatString);
+	    return liste; // on retourne la liste de chaînes de caractères après calcul
 	}
 
 	
